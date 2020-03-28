21978e20d2bd2e55602138d8ffcbe4b664edb975
gianthead97 <nikola_jankovic@tuta.io>
2020-03-29 00:38:56 +0100

    tabu

--- _solvers/tabu.py
+++ _solvers/tabu.py
@@ -1,4 +1,5 @@
 from solver import KCenterSolver
+import random
 
 
 class TabuSolver(KCenterSolver):
@@ -7,21 +8,43 @@ class TabuSolver(KCenterSolver):
 
 
     def __initalize(self, k):
-        return None
+        idx = random.sample(range(self.graph.cardV), k)
+        self.current_solution = self.graph.cardV * [False]
+        for i in idx:
+            self.current_solution[i] = True
 
     
 
 
     def get_neighbours(self):
-        neighbours = []
+        neighbours = set()
 
         transformed_solution = self.transform_solution(self.current_solution)
         for point in transformed_solution:
-            print()    
+            closest_negihbour = -1
+            closest_distance = float('inf')
+            for neigh in self.graph.get_neighbours(point):
+                if neigh[0] < closest_distance:
+                    closest_distance, closest_negihbour = neigh
+
+            neighbours.add(closest_negihbour)
+        print(neighbours)
+        return neighbours
+
+    @staticmethod  
+    def get_allowed(A:set, B:set):
+        allowed = set()
+        for a in A:
+            if a not in B:
+                allowed.add(a)
+        return random.sample(allowed, 1)
 
     def solve(self, k):
-        self.current_solution = self.__initalize(k)
+        self.__initalize(k)
         self.best_value = self.evaluate(self.current_solution)    
-        T = []
+        T = set()
         while True:
-            self.get_neighbours()
+            ind_to_change = TabuSolver.get_allowed(self.get_neighbours(), T)
+
+            print(new_solution)
+            break
